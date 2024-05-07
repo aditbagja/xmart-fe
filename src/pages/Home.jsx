fa884a4a0bda687aceafb24a2567af3a306a8169
aditbagja <aditbagja44@gmail.com>
2024-05-07 16:07:58 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="w-full h-full bg-slate-100">
+      <div className="container mx-auto">Home Page</div>
+    </section>
+  );
+};
+
+export default Home;
