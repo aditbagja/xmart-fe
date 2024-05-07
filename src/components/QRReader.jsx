199ec9691afc36e47b4c4d544be2b9f2d7c2180f
aditbagja <aditbagja44@gmail.com>
2024-05-07 17:14:41 +0700

    qr scanner screen

---
+++ src/components/QRReader.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from "react";
+import QrScanner from "qr-scanner";
+import QRFrame from "../assets/qr-frame.svg";
+import { useNavigate } from "react-router-dom";
+
+const QRReader = () => {
+  const navigate = useNavigate();
+
+  const scanner = useRef();
+  const videoEl = useRef(null);
+  const qrBoxEl = useRef(null);
+  const [qrOn, setQrOn] = useState(true);
+
+  const [scannedResult, setScannedResult] = useState("");
+
+  const onScanSuccess = (result) => {
+    setScannedResult(result.data);
+  };
+
+  const onScanFail = (error) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    if (videoEl.current && !scanner.current) {
+      scanner.current = new QrScanner(videoEl.current, onScanSuccess, {
+        onDecodeError: onScanFail,
+        preferredCamera: "environment",
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        overlay: qrBoxEl.current,
+      });
+
+      scanner?.current
+        ?.start()
+        .then(() => setQrOn(true))
+        .catch((error) => {
+          if (error) setQrOn(false);
+        });
+    }
+
+    if (!videoEl?.current) {
+      scanner.current.stop();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!qrOn)
+      alert(
+        "Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload."
+      );
+  }, [qrOn]);
+
+  useEffect(() => {
+    if (scannedResult) {
+      localStorage.setItem("qrcode", scannedResult);
+
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 2000);
+    }
+  }, [navigate, scannedResult]);
+
+  return (
+    <div className="w-full lg:w-1/2">
+      <video ref={videoEl}></video>
+      <div ref={qrBoxEl} className="w-full">
+        <img
+          src={QRFrame}
+          alt="QR Frame"
+          width={256}
+          height={256}
+          className="mx-auto fill-none"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QRReader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import QRReader from "../components/QRReader";
+
 const Home = () => {
   return (
-    <section className="w-full h-full bg-slate-100">
-      <div className="container mx-auto">Home Page</div>
+    <section className="w-full py-32">
+      <div className="container mx-auto">
+        <h1 className="font-bold text-2xl lg:text-4xl text-center">
+          Scan Your QRCode Here
+        </h1>
+        <div className="flex justify-center mt-10">
+          <QRReader />
+        </div>
+      </div>
     </section>
   );
 };
